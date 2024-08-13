110ce1fb26f4c1f644e4960042a1eeea07ee6fdc
yanxxavier <yanxavierlindolfo@gmail.com>
2024-08-12 23:09:15 -0300

    Feat: perfil JS

--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -1,4 +1,37 @@
 export default function initPerfil() {
+    const perfilUserData = localStorage.getItem("userData");
+    const perfilUserObj = JSON.parse(perfilUserData);
     const inputFile = document.querySelector("#picture_input");
-    const pictureImage = document.querySelector(".picture_image")
+    const pictureImage = document.querySelector(".picture_image");
+    const pictureText = "Choose";
+    pictureImage.innerHTML = pictureText;
+
+
+    inputFile.addEventListener("change", (e) => {
+        const inputTarget = e.target;
+        const file = inputTarget.files[0];
+        
+
+        if(file) {
+
+
+            const reader = new FileReader();
+            reader.addEventListener("load", (e) => {
+                const readerTarget = e.target;
+                const img = document.createElement("img");
+                img.src = readerTarget.result;
+                img.classList.add("perfil_picture");
+                pictureImage.innerHTML = "";
+                pictureImage.appendChild(img);
+                perfilUserObj.imgSrc = readerTarget.result;
+                
+                
+            })
+            reader.readAsDataURL(file);
+            console.log(perfilUserObj)
+
+        }else {
+            pictureImage.innerHTML = pictureText;
+        } 
+    })
 }
